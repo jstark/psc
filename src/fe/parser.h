57fe577c1b96e4e43bd1bc683fe490288e86990e
John <jfourkiotis@gmail.com>
2014-03-30 17:38:50 +0300

    a fe::Parser contains a MessageProducer object and does not inherit from one.

--- src/fe/parser.h
+++ src/fe/parser.h
@@ -1,37 +1,36 @@
 #ifndef _PSC_FE_PARSER_HPP_
 #define _PSC_FE_PARSER_HPP_ 
 
-#include "msg/message.h"
 #include <tuple>
 #include <memory>
+#include "utils/fwd.h"
 
 namespace psc { 
 
-// forward decl
-namespace im {
-	class SymbolTableStack;
-	class ICode;
-}
+FWD_DECL_NS_CL(im, SymbolTableStack)
+FWD_DECL_NS_CL(im, ICode)
+FWD_DECL_NS_CL(msg, MessageProducer)
 
 namespace fe {
 
 class Token;
 
 template<typename S>
-class Parser : public msg::MessageProducer
+class Parser
 {
 public:
     typedef S scanner_type;
-    explicit Parser(scanner_type &&scanner)
-        : _scanner(std::move(scanner)) {}
+    explicit Parser(scanner_type &&scanner, msg::MessageProducer &mp)
+        : _scanner(std::move(scanner)), _mp(mp) {}
 
     virtual std::tuple<im::ICode*, std::unique_ptr<im::SymbolTableStack>> parse() = 0;
     virtual int error_count() const = 0;
 
     Token current_token() { return _scanner.current(); }
     Token next_token() { return _scanner.next(); }
-private:
+
 	scanner_type _scanner;
+    msg::MessageProducer &_mp;
 };
 
 }}
