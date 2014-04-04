88941fedddb2dfed53bcc5ef521be3e723548b56
Ioannis Fourkiotis <jsstark@gmail.com>
2014-04-05 00:03:53 +0300

    to string for ICodeNodeAttrValue

--- src/im/common.h
+++ src/im/common.h
@@ -3,13 +3,41 @@
 
 #include <boost/variant.hpp>
 #include <string>
+#include <sstream>
+#include "im/symbol_table.h"
 
 namespace psc { namespace im {
  
 class SymbolTableEntry;
     
 using ICodeNodeAttrValue = boost::variant<int, double, std::string, const SymbolTableEntry *>;
-    
+   
+class to_str_visitor : public boost::static_visitor<std::string>
+{
+public:
+	std::string operator()(int i) const
+	{
+		return std::to_string(i);
+	}
+
+	std::string operator()(double d) const
+	{
+		std::ostringstream out;
+		out << d;
+		return out.str();
+	}
+
+	std::string operator()(const std::string& s) const
+	{
+		return s;
+	}
+
+	std::string operator()(const SymbolTableEntry *p) const
+	{
+		return p->name();
+	}
+};
+
 }
 }
 
