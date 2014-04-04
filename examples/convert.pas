6d8e8aea046651e500629eaa91c250c7329cf602
Ioannis Fourkiotis <jsstark@gmail.com>
2014-04-05 00:04:34 +0300

    examples

---
+++ examples/convert.pas
@@ -0,0 +1,39 @@
+BEGIN
+    BEGIN { Temperature conversions. }
+        five := -1 + 2 - 3 + 4 + 3;
+        ratio := five/9.0;
+
+        fahr := 72;
+        cent := (fahr - 32) * ratio;
+
+        cent := 25;
+        fahr := cent/ratio + 32;
+
+        cent := 25;
+        fahr := 32 + cent / ratio;
+    END;
+
+    { Runtime division by zero error. }
+    dze := fahr/(ratio - ratio);
+
+    BEGIN { Calculate a square root using Newton's method }
+        number := 2;
+        root := number;
+        root := (number/root + root)/2;
+        root := (number/root + root)/2;
+        root := (number/root + root)/2;
+        root := (number/root + root)/2;
+        root := (number/root + root)/2;
+    END;
+
+    BEGIN { relational operators }
+    t1 := 1 < 10;
+    t2 := 2 >= 0;
+    t3 := 2.1 > 1;
+    t4 := (1 > 2) and (3 < 10);
+    t5 := t4;
+    END;
+
+    ch := 'x';
+    str:= 'hello, world'
+END.
