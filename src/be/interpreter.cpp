ab9695d5c16a5aff9af59c1c1267e834fae61a35
Ioannis Fourkiotis <jsstark@gmail.com>
2014-04-12 01:03:05 +0300

    Enable backend processing

--- src/be/interpreter.cpp
+++ src/be/interpreter.cpp
@@ -1,4 +1,5 @@
 #include "be/interpreter.h"
+#include "be/stmnt_interpreter.h"
 #include "im/icode.h"
 #include "im/symbol_table.h"
 #include "msg/message.h"
@@ -15,8 +16,10 @@ void Interpreter::process(unique_ptr<ICode> iCode, unique_ptr<SymbolTableStack> 
 	int errors = 0;
 	auto start = std::chrono::system_clock::now();
 
-	// TODO:
-
+	auto root = iCode->root();
+	StatementInterpreter stmnt_parser{ _mp };
+	auto exec_results = stmnt_parser.execute(*root, &exec_count);
+	
 	auto end = std::chrono::system_clock::now();
 	std::chrono::duration<double> elapsed = end - start;
 
